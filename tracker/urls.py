02512a32616b072ef6cf93625ac3b81549277b9d
tcarlander <tobias.carlander@wfp.org>
2011-03-14 13:31:38 +0000

    tracker. fix

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include
-
+from tracker.collector.views import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     (r'^mapper/', include('tracker.mapper.urls')),
     (r'^kml', include('tracker.kml.urls')),
     (r'^trackme/', include('tracker.collector.urls')),
+     (r'^requests.php', 'tracker.collector.views.collect')
     (r'^trackme/media/(?P<path>.*)$', 'django.views.static.serve',
     {'document_root': MEDIA_ROOT}),
 )

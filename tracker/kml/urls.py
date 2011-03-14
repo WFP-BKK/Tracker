5c57e51010b51eedebf9ad36725eefdb38301ece
tcarlander <tobias.carlander@wfp.org>
2011-03-14 13:18:58 +0000

    tracker. fix

--- tracker/kml/urls.py
+++ tracker/kml/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import  patterns
 
 urlpatterns = patterns(
                        '',
-                       (r'^/all_items/$', 'kml.views.all_points'),
-                       (r'^.kml$', 'kml.views.all_points_kml'),
-                       (r'^/paths.kml','kml.views.all_paths_kml'),
+                       (r'^/all_items/$', 'tracker.kml.views.all_points'),
+                       (r'^.kml$', 'tracker.kml.views.all_points_kml'),
+                       (r'^/paths.kml','tracker.kml.views.all_paths_kml'),
 )

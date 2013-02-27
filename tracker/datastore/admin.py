576c98c4675cd271c2ca45421b850898e2a5769f
Tobias Carlander <tobias.carlander@wfp.org>
2013-02-27 15:34:03 +0400

    Fix Position

--- tracker/datastore/admin.py
+++ tracker/datastore/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from datastore.models import UserDetail, ActionUser, Icon,CurrentPosition
+from datastore.models import UserDetail, ActionUser, Icon,CurrentPosition,Position
 
 
 

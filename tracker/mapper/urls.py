3e8e3e539a48072c6cb07c60a159a196309868b6
Tobias Carlander <tobias.carlander@wfp.org>
2013-03-03 10:04:28 +0400

    Fix 1.5 version

--- tracker/mapper/urls.py
+++ tracker/mapper/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns
 from settings import STATIC_DOC_ROOT
-from django.views.generic.simple import redirect_to, direct_to_template
+#from django.views.generic.simple import redirect_to, direct_to_template
+from django.views.generic.base import TemplateView
+
 urlpatterns = patterns( 
                        '',
-                       ( r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'map.html'} ),
-                       ( r'^new$', 'django.views.generic.simple.direct_to_template', {'template': 'map2.html'} ),
-                       ( r'^plain$', 'django.views.generic.simple.direct_to_template', {'template': 'map_plain.html'} ),
-                       ( r'^kml/$', 'django.views.generic.simple.direct_to_template', {'template': 'map.html'} ),
+                       ( r'^$', TemplateView.as_view(template_name='map.html') ),
+                       ( r'^new$', TemplateView.as_view(template_name='map2.html') ),
+                       ( r'^plain$', TemplateView.as_view(template_name='map_plain.html') ),
+                       ( r'^kml/$', TemplateView.as_view(template_name='map.html') ),
                        ( r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_DOC_ROOT} ),
 
  )

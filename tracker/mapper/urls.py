2c22d766bc0a1c440257591e88c2dbd10911774f
Tobias Carlander <tobias.carlander@wfp.org>
2013-04-04 10:57:16 +0400

    Fix Home

--- tracker/mapper/urls.py
+++ tracker/mapper/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = patterns( 
                        '',
-                       ( r'^$', TemplateView.as_view(template_name='map.html') ),
+                       ( r'^$', TemplateView.as_view(template_name='map 2.html') ),
                        ( r'^incidents$', TemplateView.as_view(template_name='incidents.html') ),
                        ( r'^upload$', TemplateView.as_view(template_name='upload.html') ),
 

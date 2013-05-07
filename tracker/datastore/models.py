ec4380c211478772c5283229b545b5500215e5f6
Tobias Carlander <tobias.carlander@wfp.org>
2013-05-07 09:52:12 +0400

    Fix Admin

--- tracker/datastore/models.py
+++ tracker/datastore/models.py
@@ -181,7 +181,7 @@ class LoggingList(models.Model):
     errorText = models.CharField(max_length=100, blank=True, null=True, help_text="")
     actionDate = models.DateTimeField( blank=True, null=True, auto_now_add=True)
     def __unicode__(self):
-        return self.reportingServer + "  " + errorText 
+        return self.reportingServer + "  " + self.errorText 
     
 class Incident(models.Model):
     user = models.ForeignKey(ActionUser)    
